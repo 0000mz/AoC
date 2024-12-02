44b6224382ac203fa48b3068b84d5a67a42b1201
muiz <muiz.dev@gmail.com>
2024-12-01 18:42:20 -0800

    Day 1: rust, part 2
    Compute the similarity score of the 2 lists

--- 2024/001/rust/main.rs
+++ 2024/001/rust/main.rs
@@ -11,6 +11,26 @@ fn parse_line(line: &str) -> Option<(i32, i32)> {
   Some((left, right))
 }
 
+fn compute_similarity(l: &[i32], r: &[i32]) {
+  let mut m: std::collections::HashMap<i32, i32> = std::collections::HashMap::new();
+  for val in r {
+    if m.contains_key(&val) {
+      *m.get_mut(&val).unwrap() += 1;
+    } else {
+      m.insert(*val, 1);
+    }
+  } 
+  let mut sim: i32 = 0;
+  for val in l {
+    let ct = match m.get(&val) {
+      Some(el) => el.clone(),
+      _ => 0
+    };
+    sim += *val * ct;
+  }
+  println!("similarity: {}", sim);
+}
+
 fn compute(input_file: &str) -> std::io::Result<()> {
   let contents = std::fs::read_to_string(input_file)?;
   let lines: Vec<_> = contents.split("\n").collect::<_>();
@@ -45,6 +65,7 @@ fn compute(input_file: &str) -> std::io::Result<()> {
 
   let distance_sum: i32 = diffs.into_iter().sum();
   println!("Distance sum: {}", distance_sum);
+  compute_similarity(&left[..], &right[..]);
   Ok(())
 }
 
